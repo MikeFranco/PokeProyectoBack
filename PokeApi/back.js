1d7f4d5a150a6bb4ba47fe4490fc43db6797947e
Miguel Angel Franco <mfranco_98@yahoo.com>
2019-01-20 10:23:03 -0600

    The promise return image, id, name an one move of the pokemon

--- PokeApi/back.js
+++ PokeApi/back.js
@@ -2,18 +2,22 @@ const pokedex = require('pokedex-promise-v2')
 const P = new pokedex();
 
 const { sendE } = require('../mixins/response-mixins')
-
+ 
 const getPokemon = (req,res) =>{
 
   //ultimo pokemon valido: 802
-  P.getPokemonByName('8')
+  P.getPokemonByName('3')
 
   .then((response) => {
-    res.send(response.name);
+    res.send(`\t  Imagen del pokemon: ${response.sprites.front_default}
+      El Id del pokemon es: ${response.id}
+      El nombre del pokemon es: ${response.name}
+      Un movimiento de '${response.name}' es: ${response.moves[0].move.name}`
+    )   
   })
   .catch((responseError) => {
     //console.log(`Código del error: ${res.response.status}\n${res.response.statusText}`);
-    sendE(res, 404, 'El pokemon aún no nace :(')
+    sendE(res, 404, 'El pokemon aún no está registrado en el Pokedex :c </3',`El pokemon con id: $`)
     //res.send(res.response.status)
 
   });
